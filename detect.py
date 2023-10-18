e77d2d11906b099451d492919a7541c2d1d0d760
Amit Hegde <hegdeamit6@gmail.com>
2023-10-18 22:31:05 +0530

    Update GPS_Tracker
    Co-Authored-By: Deekshitha R <123435717+DeekshiR@users.noreply.github.com>

--- detect.py
+++ detect.py
@@ -128,6 +128,7 @@ def detect(save_img=False):
 
                     if save_img or view_img:  # Add bbox to image
                         label = f'{names[int(cls)]} {conf:.2f}'
+                        print(f"Confidence level: {conf:.2f}") 
                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
 
             # Print time (inference + NMS)
